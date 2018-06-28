Save ROI centroids and traces; Fix super slow traces calculation

controller.py
@@ -1753,6 +1753,8 @@ class ProcessVideosThread(QThread):
 
             rois = self.rois[:]
 
+            print(np.unique(self.rois))
+
             if self.motion_correct:
                 vid = mc_video
             else:
@@ -1810,15 +1812,15 @@ class ProcessVideosThread(QThread):
                             rois[z][y_shift:, :] = 0
                             rois[z][:, x_shift:] = 0
 
-                print("Saving ROI image of z plane {}...".format(z))
+                # print("Saving ROI image of z plane {}...".format(z))
 
-                adjusted_image = utilities.calculate_adjusted_image(mean_images[z], self.params['contrast'], self.params['gamma'])
+                # adjusted_image = utilities.calculate_adjusted_image(mean_images[z], self.params['contrast'], self.params['gamma'])
 
-                rgb_image = cv2.cvtColor(utilities.normalize(adjusted_image, video_max), cv2.COLOR_GRAY2RGB)
+                # rgb_image = cv2.cvtColor(utilities.normalize(adjusted_image, video_max), cv2.COLOR_GRAY2RGB)
 
-                roi_image, _ = utilities.draw_rois(rgb_image, rois[z], None, None, [], None, roi_overlay=None)
+                # roi_image, _ = utilities.draw_rois(rgb_image, rois[z], None, None, [], None, roi_overlay=None)
 
-                cv2.imwrite(os.path.join(video_dir_path, 'z_{}_rois.png'.format(z)), roi_image)
+                # cv2.imwrite(os.path.join(video_dir_path, 'z_{}_rois.png'.format(z)), roi_image)
 
             np.save(os.path.join(video_dir_path, 'all_rois.npy'), rois)
 
@@ -1831,29 +1833,59 @@ class ProcessVideosThread(QThread):
                 np.save(os.path.join(video_dir_path, 'z_{}_rois.npy'.format(z)), rois[z])
 
                 print("Calculating ROI activities for z={}...".format(z))
-                vid_z = vid[:, z, :, :].transpose(1, 2, 0)
-                roi_nums = np.unique(rois[z])
-                last_percent_printed = 0
-                for i in range(len(roi_nums)):
-                    l = roi_nums[i]
-                    percent = int((i/len(roi_nums))*100.0)
-                    if percent % 10 == 0 and percent > last_percent_printed:
-                        print("{}% done.".format(percent))
-                        last_percent_printed = percent
-                    activity = utilities.calc_activity_of_roi(rois[z], vid_z, l, z=z)
+                centroids, traces = utilities.calculate_centroids_and_traces(rois[z], vid[:, z, :, :])
 
-                    results[z][l] = activity
-
-                # add CSV saving here
                 print("Saving CSV for z={}...".format(z))
+
+                roi_nums = np.unique(rois[z]).tolist()
+                # remove ROI #0 (this is the background)
+                try:
+                    index = roi_nums.index(0)
+                    del roi_nums[index]
+                except:
+                    pass
+
                 with open(os.path.join(video_dir_path, 'z_{}_traces.csv'.format(z)), 'w') as file:
                     writer = csv.writer(file)
 
-                    writer.writerow(['ROI #'] + [ 'Frame {}'.format(i) for i in range(video.shape[0]) ])
+                    writer.writerow([''] + [ "ROI #{}".format(roi) for roi in roi_nums ])
 
-                    for l in np.unique(self.rois[z])[1:]:
-                        writer.writerow([l] + results[z][l].tolist())
+                    for i in range(traces.shape[0]):
+                        writer.writerow([i+1] + traces[i].tolist())
+
+                with open(os.path.join(video_dir_path, 'z_{}_centroids.csv'.format(z)), 'w') as file:
+                    writer = csv.writer(file)
+
+                    writer.writerow(['Label', 'X', 'Y'])
+
+                    for i in range(centroids.shape[0]):
+                        writer.writerow(["ROI #{}".format(roi_nums[i])] + centroids[i].tolist())
+
                 print("Done.")
+
+                # vid_z = vid[:, z, :, :].transpose(1, 2, 0)
+                # roi_nums = np.unique(rois[z])
+                # last_percent_printed = 0
+                # for i in range(len(roi_nums)):
+                #     l = roi_nums[i]
+                #     percent = int((i/len(roi_nums))*100.0)
+                #     if percent % 10 == 0 and percent > last_percent_printed:
+                #         print("{}% done.".format(percent))
+                #         last_percent_printed = percent
+                #     activity = utilities.calc_activity_of_roi(rois[z], vid_z, l, z=z)
+
+                #     results[z][l] = activity
+
+
+                # print("Saving CSV for z={}...".format(z))
+                # with open(os.path.join(video_dir_path, 'z_{}_traces.csv'.format(z)), 'w') as file:
+                #     writer = csv.writer(file)
+
+                #     writer.writerow(['ROI #'] + [ 'Frame {}'.format(i) for i in range(video.shape[0]) ])
+
+                #     for l in np.unique(self.rois[z])[1:]:
+                #         writer.writerow([l] + results[z][l].tolist())
+                # print("Done.")
 
             if not self.running:
                 self.running = False

utilities.py
@@ -24,7 +24,7 @@ import os
 import glob
 import sys
 
-from Watershed import Watershed
+# from Watershed import Watershed
 
 if sys.version_info[0] < 3:
     python_version = 2
@@ -533,6 +533,62 @@ def calculate_roi_properties(rois, mean_image):
 
     return roi_areas, roi_circs, roi_edges
 
+def calculate_centroids_and_traces(rois, video):
+    roi_nums = np.unique(rois).tolist()
+
+    # print(roi_nums)
+
+    # remove ROI #0 (this is the background)
+    try:
+        index = roi_nums.index(0)
+        del roi_nums[index]
+    except:
+        pass
+
+    n_rois = len(roi_nums)
+
+    centroids = np.zeros((n_rois, 2))
+    traces = np.zeros((video.shape[0], n_rois))
+
+    # traces[1:, 0] = np.arange(video.shape[0])
+
+    for i in range(n_rois):
+        roi = roi_nums[i]
+        mask = rois == roi
+
+        contours = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+        if len(contours) > 0:
+            contour = contours[0]
+
+            M = cv2.moments(contour)
+            center_x = int(M["m10"] / M["m00"])
+            center_y = int(M["m01"] / M["m00"])
+
+            centroids[i] = [center_x, center_y]
+
+        # traces[0, i+1]  = roi
+        # a = np.ma.array(video, mask=np.repeat(np.invert(mask)[np.newaxis, :, :], video.shape[0], axis=0))
+        # traces[1:, i+1] = np.ma.average(a, axis=(1, 2))
+
+
+        coords = np.nonzero(mask)
+        min_y = min(coords[0])
+        max_y = max(coords[0])
+        min_x = min(coords[1])
+        max_x = max(coords[1])
+
+        mask = mask[min_y:max_y+1, min_x:max_x+1]
+
+        mask[mask == 0] = np.nan
+        # trace_sum = np.sum(video*mask[np.newaxis, :, :], axis=(1, 2))
+        # count = np.count_nonzero(mask)
+        # traces[1:, i+1] = trace_sum/count
+        traces[:, i] = np.nanmean(video[:, min_y:max_y+1, min_x:max_x+1]*mask[np.newaxis, :, :], axis=(1, 2))
+        # traces[1:, i+1] = np.nanmean(np.where(mask[np.newaxis, :, :], video, np.nan), axis=(1, 2))
+
+    return centroids, traces
+
 def find_rois(cells_mask, starting_image, mean_image):
     # w = Watershed()
     # rois = w.apply(equalized_image)
@@ -547,7 +603,7 @@ def filter_rois(image, rois, min_area, max_area, min_circ, max_circ, min_edge_co
     filtered_out_rois = []
 
     for l in np.unique(rois):
-        if ((not (min_area <= roi_areas[l-1] <= max_area)) or (not (min_circ <= roi_circs[l-1] <= max_circ)) or (not (min_edge_contrast <= roi_edges[l-1])) or l <= 1) and l not in locked_rois:
+        if ((not (min_area <= roi_areas[l-1] <= max_area)) or (not (min_circ <= roi_circs[l-1] <= max_circ)) or l <= 1) and l not in locked_rois:
             mask = rois == l
             
             filtered_rois[mask] = 0
@@ -650,7 +706,7 @@ def draw_rois(rgb_image, rois, selected_roi, erased_rois, filtered_out_rois, loc
         roi_overlay = np.zeros(image.shape)
 
         for l in np.unique(rois):
-            if l < 1 or l in filtered_out_rois or l in erased_rois:
+            if (filtered_out_rois is not None and erased_rois is not None) and (l < 1 or l in filtered_out_rois or l in erased_rois):
                 continue
 
             mask = rois == l